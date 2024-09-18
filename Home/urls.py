2eb4e0b62a1252c330a02a1392ab98192e5f3ef4
RAKESH_MAHAT <81910340+rakeshmahat25@users.noreply.github.com>
2024-09-18 11:35:15 +0545

    Add files via upload

--- Home/urls.py
+++ Home/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('destination', views.destination, name='destination'),
+    path('about/', views.about, name='about'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contact/', views.contact, name='contact'),
+
 ]
--- Home/views.py
+++ Home/views.py
@@ -5,3 +5,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def destination(request):
+    return render(request, 'destination.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request, 'contact.html')
