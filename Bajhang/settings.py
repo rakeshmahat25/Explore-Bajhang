1095723975f3837f5c7ec1ade31b2260f937d530
RAKESH_MAHAT <81910340+rakeshmahat25@users.noreply.github.com>
2024-09-18 14:12:31 +0545

    Update settings.py

--- Bajhang/settings.py
+++ Bajhang/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+#DEBUG = config('DEBUG')
+DEBUG = True
+
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
